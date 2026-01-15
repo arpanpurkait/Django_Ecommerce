e0adf3bdf51adbde40ec10aea58391907fd43d9f
Arpan Purkait <arpanpurkait743354@gmail.com>
2026-01-15 11:37:25 +0530

    added till 3:45

--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Cart, CartItem   # adjust model names if needed
 
-# Register your models here.
+admin.site.register(Cart)
+admin.site.register(CartItem)
